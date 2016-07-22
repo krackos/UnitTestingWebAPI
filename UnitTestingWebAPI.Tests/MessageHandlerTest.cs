2b378b5b80de822f2bf62a44c8a244b546db4c6f
teclo.lozano <teclo.lozano@LSTKEN52128>
2016-07-22 16:34:40 -0700

    Testing sfd

---
+++ UnitTestingWebAPI.Tests/MessageHandlerTest.cs
@@ -0,0 +1,22 @@
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnitTestingWebAPI.API.Core.Controllers;
+using UnitTestingWebAPI.Data;
+using UnitTestingWebAPI.Data.Infrastructure;
+using UnitTestingWebAPI.Data.Repositories;
+using UnitTestingWebAPI.Domain;
+using UnitTestingWebAPI.Services;
+using System.Web.Http.Results;
+using System.Web.Http;
+using System.Net.Http;
+using System.Net;
+using System.Web.Http.Routing;
+
+namespace UnitTestingWebAPI.Tests
+{
+    [TestFixture]
+    public class MessageHandlerTest {}
+}
